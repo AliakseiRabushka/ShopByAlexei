c004196e18e5e9905c1c50107ec461a968ef3c9a
AliakseiRabushka <ljo6e4kim@gmail.com>
2024-02-14 14:22:44 +0300

    continue pet project

--- src/js/app.js
+++ src/js/app.js
@@ -1,26 +1,63 @@
 import Alpine from "../../node_modules/alpinejs/dist/module.esm.js";
 window.Alpine = Alpine;
-Alpine.start();
+
 import {
   createStore,
-  createEffect,
-  sample,
-} from "../../node_modules/effector/effector.mjs";
+  createEvent,
+} from "https://cdn.jsdelivr.net/npm/effector/effector.mjs";
+
+import { products } from "./products.js";
+
 console.log("Console log message");
 
-const $counter = createStore(0);
-const increaseByOne = createEffect();
+console.log(products);
+const $products = createStore(products);
 
-setInterval(() => {
-  increaseByOne();
-}, 2000);
+const addToCart = createEvent();
+const removeFromCart = createEvent();
+const updateCart = createEvent();
 
-sample({
-  clock: increaseByOne,
-  source: $counter,
-  fn: (counter) => {
-    console.log("Counter", counter);
-    return counter + 1;
-  },
-  target: $counter,
+const $cart = createStore([])
+  .on(addToCart, (state, item) => {
+    let id = item.id;
+    if (!state.find((item) => item.id == id)) {
+      return [...state, item];
+    }
+    alert("Product has already been added to cart !");
+    return [...state];
+  })
+  .on(removeFromCart, (item, id) => {
+    let state = item.filter((item) => item.id !== id);
+    return state;
+  })
+  .on(updateCart, (item, quantity) => {
+    console.log(quantity);
+  });
+
+$cart.watch((state) => {
+  console.log("state", state);
 });
+
+export const actions = {
+  addToCart,
+  removeFromCart,
+  updateCart,
+};
+
+Alpine.store("products", {
+  init() {
+    this.products = products;
+  },
+});
+
+Alpine.store("cart", {
+  init() {
+    this.actions = { addToCart, removeFromCart, updateCart };
+
+    $cart.watch((store) => {
+      this.store = store;
+    });
+  },
+});
+
+Alpine.start();
---
+++ src/js/products.js
@@ -0,0 +1,29 @@
+export const products = [
+  {
+    id: 1,
+    name: "Pixel 8 Pro",
+    quantity: 1,
+    description:
+      'Screen: 6.81 "1080x2400 pixels, IPS Processor: MediaTek Helio G99, 8 cores, 2.2 GHz Memory: RAM 8 GB, 256 GB Physical SIM card format: Nano Resolution of the main camera module: 108 MP',
+    price: 450,
+    image: "../src/images/pixel_8_pro.jpeg",
+  },
+  {
+    id: 2,
+    name: "Apple iPhone 15 Pro",
+    quantity: 1,
+    description:
+      'Apple iOS, 6.1" OLED screen (1179x2556) 120 Hz, Apple A17 Pro, 8 GB RAM, 256 GB memory, 48 MP camera, 3274 mAh battery, 1 SIM (nano-SIM/eSIM), IP68 waterproof',
+    price: 1300,
+    image: "../src/images/Apple_iPhone_15_pro.png",
+  },
+  {
+    id: 3,
+    name: "Samsung Galaxy A52",
+    quantity: 1,
+    description:
+      'Android, 6.5" AMOLED screen (1080x2400) 90 Hz, Qualcomm Snapdragon 720G, 4 GB RAM, 128 GB memory, memory card support, 64 MP camera, 4500 mAh battery, 2 SIM (nano-SIM), IP67 waterproof',
+    price: 280,
+    image: "../src/images/samsung_galaxy_a52.png",
+  },
+];
